5502abc2402ead4fc4e3945405ea4eb3fa6bf44c
José Junior <junioradorart@gmail.com>
2019-02-22 17:56:47 -0300

    change to local audio files

--- js/drumscontrol.js
+++ js/drumscontrol.js
@@ -1,9 +1,13 @@
 var sound = new jAudio();
 var drum = [];
 
-drum['KeyA'] = new jAudio('http://www.soundjig.com/mp3/soundfx/drumkits/SD0010.mp3');
-drum['KeyS'] = new jAudio('http://www.soundjig.com/mp3/soundfx/drumkits/BD0000.mp3');
-drum['KeyJ'] = new jAudio('http://www.soundjig.com/mp3/soundfx/drumkits/CH.mp3');
+// drum['KeyA'] = new jAudio('http://www.soundjig.com/mp3/soundfx/drumkits/SD0010.mp3');
+// drum['KeyS'] = new jAudio('http://www.soundjig.com/mp3/soundfx/drumkits/BD0000.mp3');
+// drum['KeyJ'] = new jAudio('http://www.soundjig.com/mp3/soundfx/drumkits/CH.mp3');
+
+drum['KeyA'] = new jAudio('/sounds/snare.mp3');
+drum['KeyS'] = new jAudio('/sounds/kick.mp3');
+drum['KeyJ'] = new jAudio('/sounds/hithat.mp3');
 
 document.addEventListener('keydown', function(e) {
   var url = '';
